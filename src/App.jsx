ac7f452bddc98f809a9b613635b6bcc0d78f361d
fatima <fatma.anime.taha@gmail.com>
2024-10-14 14:17:41 +0300

    add icon in footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./component/navbar";
 
 function App() {
   return (
-    <div className="bg-black h-screen text-white font-oswald">
+    <div className="bg-black h-screen font-oswald">
       <Navbar/>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, veniam laudantium? Ut iusto commodi autem repellendus laborum adipisci? Rerum, nobis animi? Quis veniam voluptatum tenetur cupiditate qui sequi incidunt voluptas!
       <Footer/>
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { AiOutlineFacebook } from "react-icons/ai";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { TfiEmail } from "react-icons/tfi";
+import { GrPhone } from "react-icons/gr";
+import { IoLocationOutline } from "react-icons/io5";
 
 function Footer() {
     return (
         <div>
         <div
-            className="max-w-screen- flex flex-wrap items-center justify-between mx-auto p-1 rounded-t-md "
-            style={{
-            backgroundImage:
-                "linear-gradient(-45deg, #5A4FCF, #f1f2f6, #FF3503, #FFD700)",
-            }}
-        ></div>
+            className="max-w-screen- flex flex-wrap items-center justify-between mx-auto p-1 rounded-t-md bg-[#f1f2f6]"
+            
+        >
+            <div className="footer-text"> <FaInstagram /> <AiOutlineFacebook /> <AiOutlineLinkedin /><TfiEmail /> <GrPhone /> <IoLocationOutline />All copyright are Reserved@.</div>
+        </div>
+        
+       
         </div>
     );
 }
