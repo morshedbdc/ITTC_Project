7c1c440466bd2073dbd34709ed80f77c1093750e
omnia-adnan <o.m.a.1514.ja@gmail.com>
2024-10-22 13:03:41 +0300

    create home page

--- src/Sections/AboutUs.jsx
+++ src/Sections/AboutUs.jsx
@@ -4,9 +4,8 @@ import Image from "../component/Image";
 function AboutUs() {
   return (
     <div className="md:flex">
-      <Image />
       <div className="container md:my-24 xl:my-32 px-2 py-2">
-        <h2 className="text- xl:-ml-10 xl:text-xl text-gray-800 w-full xl:w-10/12 p-2 md:p-2">
+        <h2 className="max-w-2xl mb-6 lg:mb-8 md:text-lg lg:text-xl md:ml-4 text-gray-800">
         ITTC offers a wide range of courses that cover the latest technologies and trends in IT, 
         ensuring that professionals can improve their technical skills and stay up-to-date with 
         the rapidly evolving technological landscape. We strive to empower both individuals and 
@@ -14,6 +13,7 @@ function AboutUs() {
         an ever-changing technological environment.
         </h2>
         </div>
+      <Image />
     </div>
   );
 }
